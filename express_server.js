e557223ed93d9b906a780c50ceceef15b2e08df8
ElenaCherpakova <elena.cherpakova@gmail.com>
2021-12-07 04:40:10 +0000

    shortening_part2

--- express_server.js
+++ express_server.js
@@ -1,21 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-
-
-const generateRandomString = function() {
+const generateRandomString = function () {
   return Math.random().toString(36).substr(2, 6);
 };
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -27,8 +24,16 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get("/", (req, res) => {
@@ -52,12 +57,14 @@ app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  console.log(req.body); // Log the POST request body to the console
+  res.redirect(`/urls/${shortURL}`); // Respond with 'Ok' (we will replace this)
 });
 
-app.post("/urls", (req, res) => { 
-  generateRandomString();
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
